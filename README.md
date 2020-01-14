8cb5c409c80047dc3ad05d1781d4c798861536a3
Timothy Marois <timothymarois@gmail.com>
2020-01-14 18:38:06 -0500

    Initial build to the SDK

---
+++ README.md
@@ -0,0 +1,3 @@
+# TD Ameritrade PHP SDK
+
+This package acts as the PHP SDK for the [TD Ameritrade API](https://developer.tdameritrade.com/apis).
---
+++ src/TDAmeritrade.php
@@ -0,0 +1,31 @@
+<?php namespace TD;
+
+class TDAmeritrade
+{
+
+    /**
+     * API Real Path
+     *
+     * @var array
+     */
+    private $apiPath = 'https://api.tdameritrade.com';
+
+    /**
+     * API Paths
+     *
+     * @var array
+     */
+    private $paths = [
+        "accounts"     => "/v1/accounts"
+    ];
+
+    /**
+     * Set Alpaca 
+     *
+     */
+    public function __construct()
+    {
+
+    }
+
+}
